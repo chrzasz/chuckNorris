cde430a0d3db958ce26174535eabebb374f745a7
Grzegorz Chrzaszczyk <chrzaszczyk@gmail.com>
2018-12-26 13:42:23 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Chuck Norris jokes
+
+Retrieve a random chuck joke in JSON format from:
+
+[api.chucknorris.io]https://api.chucknorris.io/
---
+++ src/main/java/httpClient/ChuckNorrisIO.java
@@ -0,0 +1,7 @@
+package httpClient;
+
+/**
+ * Created by Grzegorz Chrzaszczyk on 26-12-2018  01:37 PM
+ */
+public class ChuckNorrisIO {
+}
